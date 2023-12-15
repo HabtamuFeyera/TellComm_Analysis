2416b71b3368fae3779d33242f87e53c63440fcb
HabtamuFeyera <habtamufeyer02@gmail.com>
2023-12-14 21:39:44 -0800

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from sklearn import datasets
 
 st.title('wel come to streamlit')
 
@@ -8,5 +9,21 @@ st.write("""
 I select the best one here
          
 """)
-mydataset = st.sidebar.selectbox("Select Dataset",("Iris","Breast cancer","winne dataset"))
-st.write(mydataset)
+my_dataset = st.sidebar.selectbox("Select Dataset",("Iris","Breast Cancer","wine dataset"))
+my_classifier = st.sidebar.selectbox("Select Classifier",("KNN","SVm","Random Forest"))
+
+def get_dataset(my_dataset):
+    if my_dataset == "Iris":
+        data = datasets.load_iris()
+    elif my_dataset == "Breast Cancer":
+        data = datasets.load_breast_cancer()
+    else:
+        data = datasets.load_wine()
+
+    x = data.data
+    y = data.target
+    return x,y
+
+x,y = get_dataset(my_dataset)
+
+
