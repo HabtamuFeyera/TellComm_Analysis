f76d2b17b6e93afb4173f9768dbe569eb7fadfa6
HabtamuFeyera <habtamufeyer02@gmail.com>
2023-12-14 22:00:41 -0800

    modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,71 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
 from sklearn import datasets
+from sqlalchemy import create_engine
 
-st.title('wel come to streamlit')
+# Database connection parameters
+db_params = {
+    'dbname': 'week1',
+    'user': 'postgres',
+    'password': 'habte',
+    'host': 'localhost',
+    'port': '5432'
+}
+
+# Create a SQLAlchemy engine
+engine = create_engine(f'postgresql+psycopg2://{db_params["user"]}:{db_params["password"]}@{db_params["host"]}:{db_params["port"]}/{db_params["dbname"]}')
+
+# SQL query to retrieve data
+sql_query = "SELECT * FROM xdr_data;"
+
+# Read data from PostgreSQL into a DataFrame
+mydata = pd.read_sql_query(sql_query, engine)
+
+st.title('Welcome to Streamlit')
 
 st.write("""
          
-# Explore mlops
-I select the best one here
+# Explore MLOps
+  Select the best one here
          
 """)
-my_dataset = st.sidebar.selectbox("Select Dataset",("Iris","Breast Cancer","wine dataset"))
-my_classifier = st.sidebar.selectbox("Select Classifier",("KNN","SVm","Random Forest"))
+my_dataset = st.sidebar.selectbox("Select Dataset", ("Iris", "Breast Cancer", "Wine Dataset", "Custom", "PostgreSQL Data"))
 
-def get_dataset(my_dataset):
-    if my_dataset == "Iris":
-        data = datasets.load_iris()
-    elif my_dataset == "Breast Cancer":
-        data = datasets.load_breast_cancer()
+if my_dataset == "Custom":
+    uploaded_file = st.sidebar.file_uploader("Upload a CSV file", type=["csv"])
+    if uploaded_file is not None:
+        data = pd.read_csv(uploaded_file)
+        x = data.iloc[:, :-1].values
+        y = data.iloc[:, -1].values
+
+        st.write("Custom Dataset:")
+        st.write("First 5 rows of the dataset:")
+        st.write(data.head())
+
     else:
-        data = datasets.load_wine()
+        st.warning("Please upload a CSV file.")
+        st.stop()
 
-    x = data.data
-    y = data.target
-    return x,y
+elif my_dataset == "PostgreSQL Data":
+    st.write("PostgreSQL Dataset:")
+    st.write("First 5 rows of the dataset:")
+    st.write(mydata.head())
 
-x,y = get_dataset(my_dataset)
-st.write("shape of dataset", x.shape)
-st.write("number of classs", len(np.unique(y)))
+else:
+    def get_dataset(my_dataset):
+        if my_dataset == "Iris":
+            data = datasets.load_iris()
+        elif my_dataset == "Breast Cancer":
+            data = datasets.load_breast_cancer()
+        else:
+            data = datasets.load_wine()
 
+        x = data.data
+        y = data.target
+        return x, y
+
+    x, y = get_dataset(my_dataset)
+
+    st.write("Shape of dataset", x.shape)
+    st.write("Number of classes", len(np.unique(y)))
